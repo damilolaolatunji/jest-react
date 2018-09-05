c1abea0d0511109beeb042c2b7d2727fe6da5c67
Ayo Isaiah <ayisaiah@gmail.com>
2018-09-05 11:29:59 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Testing React applications with Jest and Enzyme
+
+A repository written for a blog post about testing React with Jest.
+
+## Requirements
+
+- [Node.js](https://nodejs.org/en/download/)
+
+## Getting Started
+
+1. Clone this repository and `cd` into it.
+2. Execute `npm install` to download dependencies.
+3. Run `yarn tests` to run the tests with Jest.
+
+## Licence
+
+[MIT](https://opensource.org/licenses/MIT)
+
---
+++ src/App.test.js
@@ -0,0 +1,33 @@
+import renderer from 'react-test-renderer';
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App';
+
+describe('App component', () => {
+  it('starts with a count of 0', () => {
+    const wrapper = shallow(<App />);
+    const text = wrapper.find('p').text();
+    expect(text).toEqual('Count: 0');
+  });
+
+  it('increments count by 1 when the increment button is clicked', () => {
+    const wrapper = shallow(<App />);
+    const incrementBtn = wrapper.find('button.increment');
+    incrementBtn.simulate('click');
+    const text = wrapper.find('p').text();
+    expect(text).toEqual('Count: 1');
+  });
+
+  it('decrements count by 1 when the decrement button is clicked', () => {
+    const wrapper = shallow(<App />);
+    const decrementBtn = wrapper.find('button.decrement');
+    decrementBtn.simulate('click');
+    const text = wrapper.find('p').text();
+    expect(text).toEqual('Count: -1');
+  });
+
+  it('matches the snapshot', () => {
+    const tree = renderer.create(<App />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
